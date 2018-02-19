dd8fda43a23b9a9e0064decdbf71afe07158fc54
Michał Bachowski <michal@bachowski.pl>
2018-02-19 16:08:41 +0100

    Add setup.py

--- src/logging_utils/__init__.py
+++ src/logging_utils/__init__.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 from __future__ import absolute_import
 
-from ._version import __version__
 from .logging import getLogger
 from .sentinel import SentinelBuilder
 
-__all__ = ['__version__', 'getLogger', SentinelBuilder]
+__all__ = ['getLogger', 'SentinelBuilder']
 
--- src/logging_utils/_version.py
+++
@@ -1,4 +0,0 @@
-# encoding: utf-8
-
-__version__ = '0.0.1'
-VERSION = __version__
